3251fc84ca55246da07cdf126a0130322bdfe1e4
KalekidanBekele <kalekidanen.73@gmail.com>
2023-05-09 22:31:41 -0400

    added additional heroku changes

--- index.js
+++ index.js
@@ -328,6 +328,8 @@ app.get('/logout', (req, res) => {
 });
 
 //Start Server
-app.listen( process.env.PORT || 3000, () => {
-    console.log('Server started on port' + this.address().port, app.settings.env);
+const PORT = process.env.PORT || 3000;
+
+app.listen( PORT, () => {
+    console.log('Server started on port', PORT);
 });
