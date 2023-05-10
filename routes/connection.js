ec7518d9db9f627635dfc030a135b3a25b16036a
KalekidanBekele <kalekidanen.73@gmail.com>
2023-05-10 00:04:13 -0400

    made additional changes for Heroku

--- routes/connection.js
+++ routes/connection.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-   'bankproject',
-   'root',
-   '',
+   'heroku_2a38526b70ff8a4', //'bankproject',
+   'baa6318898e6a2', //'root',
+   'a559d61a', //'',
     {
-      host: 'localhost',
-      dialect: 'mysql'
+      host: 'us-cdbr-east-06.cleardb.net', //'localhost',
+      dialect: 'mysql',
+      createDatabaseIfNotExist: true
     }
   );
 
